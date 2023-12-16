ce939f61880cecbec08ebfee88fe76c7fea59a65
VladyslavStolbov <stolbovvladyslavcg@gmail.com>
2023-12-16 09:22:58 +0200

    changed images for game

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+from config import *
 from pygame.locals import *
 
 
@@ -9,11 +10,16 @@ class Paddle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.position_x = position_x
         self.position_y = position_y
-        self.rect.center = (position_x, position_y)
+        self.rect.topleft = (position_x, position_y)
         self.image.set_colorkey("black")
 
     def update(self, keys):
         if any(keys[key] for key in (K_UP, K_w, K_k)):
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -speed)
         if any(keys[key] for key in (K_DOWN, K_s, K_j)):
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, speed)
+
+        if self.rect.topleft < (self.position_x, 0):
+            self.rect.topleft = (self.position_x, 0)
+        if self.rect.bottomleft > (self.position_x, SCREEN_HEIGHT):
+            self.rect.bottomleft = (self.position_x, SCREEN_HEIGHT)
